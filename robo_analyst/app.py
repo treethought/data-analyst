1a97164486142b3d475d0f3b358f44fb3d9e250f
Cam Sweeney <cpsweene@gmail.com>
2017-05-31 01:48:50 -0700

    flask static and template folders

--- robo_analyst/app.py
+++ robo_analyst/app.py
@@ -12,7 +12,10 @@ def create_app(config_object=ProdConfig):
 
     :param config_object: The configuration object to use.
     """
-    app = Flask(__name__.split('.')[0])
+    app = Flask(__name__.split('.')[0],
+                static_folder='web/static',
+                template_folder='web/templates')
+
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
